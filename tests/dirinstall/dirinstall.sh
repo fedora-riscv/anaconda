d819f572431c024a4cc0414b1599488da4491e3b
Radek Vykydal <rvykydal@redhat.com>
2021-09-08 15:14:54 +0200

    Store tmt tests in tests directory

--- tests/dirinstall/dirinstall.sh
+++ tests/dirinstall/dirinstall.sh
@@ -9,8 +9,8 @@ WORK_DIR=$(mktemp -d /var/tmp/dirinstall.XXXXXX)
 # Create kickstart
 
 KICKSTART_PATH=${WORK_DIR}/ks.cfg
-source plans/repositories
-TEST_KICKSTART=plans/ks.dirinstall.cfg
+source ./repositories
+TEST_KICKSTART=./ks.dirinstall.cfg
 
 # Dump URLs of installation repositories found in local repositories whose names are configured in 'repositories' file
 echo "url --metalink=$(dnf repoinfo $BASE_REPO | grep ^Repo-metalink | cut -d: -f2- | sed 's/^ *//')" > ${KICKSTART_PATH}
@@ -35,3 +35,8 @@ anaconda --dirinstall ${INSTALL_DIR} --kickstart ${KICKSTART_PATH} --${ANACONDA_
 # Remove test work directory
 
 rm -rf ${WORK_DIR}
+
+
+# Show and remove the logs for this anaconda run
+
+./show_logs.sh
